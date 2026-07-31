2b7c6386ec3f413dea593af1ea38fb6020bc5168
Silentboy07A <sakthibalamurugan06@gmail.com>
2026-07-31 10:04:07 +0530

    loops

---
+++ day1/grade.py
@@ -0,0 +1,12 @@
+m =int(input("Enter mark:"))
+if m>=90:
+    print("Grade is A")
+elif m>=80:
+    print("Grade is B")
+elif m>=60:
+    print("Grade is C")
+elif m>=50:
+    print("pass")
+else:
+    print("fail")
+    
