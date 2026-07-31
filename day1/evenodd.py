5a39991b397aa74db458108bf9bcdc772b7d90c9
Silentboy07A <sakthibalamurugan06@gmail.com>
2026-07-31 09:49:06 +0530

    if else

---
+++ day1/evenodd.py
@@ -0,0 +1,7 @@
+a =int (input("enter a no:"))
+if a%2 ==0:
+    print("even")
+else:
+    print("odd")
+     
+        
--- day1/pass.py
+++ day1/pass.py
@@ -1,3 +1,5 @@
-marks =int(input("Enter mark"))
+marks =int(input("Enter mark:"))
 if marks>=35:
     print("pass")
+else:
+    print("fail")
