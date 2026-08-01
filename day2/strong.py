f00b4aea3d97953bcb5410ff7c88b6dffbce45d7
Silentboy07A <sakthibalamurugan06@gmail.com>
2026-08-01 10:28:30 +0530

    strong no

---
+++ day2/strong.py
@@ -0,0 +1,21 @@
+n = int(input("Enter a number"))
+
+o = n
+s = 0
+
+while n > 0:
+    digit = n % 10
+
+    fact = 1
+
+    for i in range(1, digit + 1):
+        fact *= i
+
+    s += fact
+
+    n //= 10
+
+if o == s:
+    print("Strong Number")
+else:
+    print("Not Strong Number")
