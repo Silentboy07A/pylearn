2f8440fa1b6fb125f3063381d68e6d8530142009
Silentboy07A <sakthibalamurugan06@gmail.com>
2026-08-18 18:25:52 +0530

    Add Python modules and gitignore

---
+++ day13/main.py
@@ -0,0 +1,6 @@
+import maths
+print(maths.add(12,3))
+print(maths.subract(12, 3))
+print(maths.multiplication(12, 3))
+print(maths.divison(12, 3))
+print(maths.expo(12, 3))
---
+++ day13/maths.py
@@ -0,0 +1,11 @@
+def add (a,b):
+    return a+b
+def subract(a,b):
+    return a-b
+def multiplication(a,b):
+    return a*b
+def divison(a,b):
+    return a/b
+def expo(a,b):
+    return a**b
+
