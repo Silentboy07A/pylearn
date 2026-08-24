836a1aa0c29c6c7632177326ab78b7d254b42aeb
Silentboy07A <sakthibalamurugan06@gmail.com>
2026-08-24 18:07:48 +0530

    Nested dictionary indexing

---
+++ day17/ss.py
@@ -0,0 +1,10 @@
+students = {
+    "student1": {"name": "Arun", "marks": 85},
+    "student2": {"name": "Ravi", "marks": 92},
+    "student3": {"name": "Kiran", "marks": 78}
+}
+print(students["student1"]["marks"])
+print(students["student2"]["name"])
+print(students['student3']["marks"])
+
+
